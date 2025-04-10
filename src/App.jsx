6f9f07d7dda6b77c8ca83efb5f204d358b6d163d
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-04-10 21:54:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+const [state, setState] = useState(false);
   return (
     <>
-      
+      <button className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-250 hover:scale-500 transition  duration-500 cursor-pointer' onClick={()=>setState(!state)}>
+        {(state)?'❤️':'🤍'}
+      </button>
     </>
   )
 }
