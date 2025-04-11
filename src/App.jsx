b48b29b0c6d1202400f10fd06821490c4b1b2cf5
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-04-11 22:27:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const [state, setState] = useState(false);
       <button className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-250 hover:scale-500 transition  duration-500 cursor-pointer' onClick={()=>setState(!state)}>
         {(state)?'❤️':'🤍'}
       </button>
+      <a href=""></a>
     </>
   )
 }
